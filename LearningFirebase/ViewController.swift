5268c15bf8a978f0d1fad29642564693303d0c5c
Kio <kkudumu@gmail.com>
2018-01-12 16:08:10 -0800

    Initial Commit

---
+++ LearningFirebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LearningFirebase
+//
+//  Created by Kioja Kudumu on 1/12/18.
+//  Copyright © 2018 Kioja Kudumu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
