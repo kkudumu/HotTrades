7ea8835642335680388526bedf2994d066fe62f8
Kio <kkudumu@gmail.com>
2018-01-30 17:15:57 -0800

    fixing git

---
+++ LearningFirebase/Controller/FreeUserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FreeUserTableViewCell.swift
+//  LearningFirebase
+//
+//  Created by Kioja Kudumu on 1/29/18.
+//  Copyright © 2018 Kioja Kudumu. All rights reserved.
+//
+
+import UIKit
+
+class FreeUserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
