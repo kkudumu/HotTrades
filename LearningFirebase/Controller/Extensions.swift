8e120648e011dac1e16779d8683c6e36e74c887a
Kio <kkudumu@gmail.com>
2018-02-01 19:31:48 -0800

    did half of autolayout needed

--- LearningFirebase/Controller/Extensions.swift
+++ LearningFirebase/Controller/Extensions.swift
@@ -1,41 +1,42 @@
+////
+////  File.swift
+////  LearningFirebase
+////
+////  Created by Kioja Kudumu on 1/23/18.
+////  Copyright © 2018 Kioja Kudumu. All rights reserved.
+////
 //
-//  File.swift
-//  LearningFirebase
+//import UIKit
 //
-//  Created by Kioja Kudumu on 1/23/18.
-//  Copyright © 2018 Kioja Kudumu. All rights reserved.
+//var imageCache = NSCache<NSString, UIImage>()
 //
+//extension UIImageView {
+//    func loadImageUsingCacheWithUrlString(urlString: String) {
+//        
+//        self.image = nil
+//        
+//        //check cache for image first
+//        if let cachedImage = imageCache.object(forKey: urlString as NSString) {
+//            self.image = cachedImage
+//            return
+//        }
+//        
+//        
+//        let url = URL(string: urlString)
+//        URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
+//            if error != nil {
+//                print(error)
+//                return
+//            }
+//            DispatchQueue.main.async {
+//                if let downloadedImage = UIImage(data: data!) {
+//                    imageCache.setObject(downloadedImage, forKey: urlString as NSString)
+//                    self.image = downloadedImage
+//                }
+//                
+//            }
+//            
+//        }).resume()
+//    }
+//}
 
-import UIKit
-
-var imageCache = NSCache<NSString, UIImage>()
-
-extension UIImageView {
-    func loadImageUsingCacheWithUrlString(urlString: String) {
-        
-        self.image = nil
-        
-        //check cache for image first
-        if let cachedImage = imageCache.object(forKey: urlString as NSString) {
-            self.image = cachedImage
-            return
-        }
-        
-        
-        let url = URL(string: urlString)
-        URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
-            if error != nil {
-                print(error)
-                return
-            }
-            DispatchQueue.main.async {
-                if let downloadedImage = UIImage(data: data!) {
-                    imageCache.setObject(downloadedImage, forKey: urlString as NSString)
-                    self.image = downloadedImage
-                }
-                
-            }
-            
-        }).resume()
-    }
-}
