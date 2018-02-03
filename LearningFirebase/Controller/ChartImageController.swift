76f62e8a13942883755abce004609dd1b1dd81ab
Kio <kkudumu@gmail.com>
2018-02-03 01:07:46 -0800

    changed some zoom and pan features on the image

--- LearningFirebase/Controller/ChartImageController.swift
+++ LearningFirebase/Controller/ChartImageController.swift
@@ -9,14 +9,29 @@
 import UIKit
 import Firebase
 
-class ChartImageController: UIViewController {
+class ChartImageController: UIViewController, UIGestureRecognizerDelegate {
 
     @IBOutlet weak var chartImageView: UIImageView!
     var newImage: UIImage!
+    var chartImageViewOrigin: CGPoint!
+    var isZooming = false
+    var originalImageCenter:CGPoint?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       chartImageView.image = newImage
+        let scaleImage = UIPinchGestureRecognizer(target: self, action: #selector(self.scaleImage(_:)))
+        scaleImage.delegate = self
+        self.chartImageView.addGestureRecognizer(scaleImage)
+        let moveImage = UIPanGestureRecognizer(target: self, action: #selector(self.moveImage(_:)))
+        moveImage.delegate = self
+        self.chartImageView.addGestureRecognizer(moveImage)
+      
+        chartImageView.image = newImage
+        
+//        chartImageViewOrigin = chartImageView.frame.origin
+        
+        
         
 //        let gestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(moveImage(_:)))
 //        self.chartImageView.addGestureRecognizer(gestureRecognizer)
@@ -25,20 +40,63 @@ class ChartImageController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         chartImageView.image = newImage
     }
+    
+ 
 
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+    
+    
     @IBAction func scaleImage(_ sender: UIPinchGestureRecognizer) {
         
-        chartImageView.transform = CGAffineTransform(scaleX: sender.scale, y: sender.scale)
-        
+        if sender.state == .began {
+            let currentScale = self.chartImageView.frame.size.width / self.chartImageView.bounds.size.width
+            let newScale = currentScale*sender.scale
+            if newScale > 1 {
+                self.isZooming = true
+            }
+        } else if sender.state == .changed {
+            guard let view = sender.view else {return}
+            let pinchCenter = CGPoint(x: sender.location(in: view).x - view.bounds.midX,
+                                      y: sender.location(in: view).y - view.bounds.midY)
+            let transform = view.transform.translatedBy(x: pinchCenter.x, y: pinchCenter.y)
+                .scaledBy(x: sender.scale, y: sender.scale)
+                .translatedBy(x: -pinchCenter.x, y: -pinchCenter.y)
+            let currentScale = self.chartImageView.frame.size.width / self.chartImageView.bounds.size.width
+            var newScale = currentScale*sender.scale
+            if newScale < 1 {
+                newScale = 1
+                let transform = CGAffineTransform(scaleX: newScale, y: newScale)
+                self.chartImageView.transform = transform
+                sender.scale = 1
+            }else {
+                view.transform = transform
+                sender.scale = 1
+            }
+        } else if sender.state == .ended || sender.state == .failed || sender.state == .cancelled {
+            guard let center = self.originalImageCenter else {return}
+            UIView.animate(withDuration: 0.3, animations: {
+                self.chartImageView.transform = CGAffineTransform.identity
+                self.chartImageView.center = center
+            }, completion: { _ in
+                self.isZooming = false
+            })
+        }
     }
     
     @IBAction func moveImage(_ sender: UIPanGestureRecognizer) {
         
-        let translation = sender.translation(in: self.view)
-        
-        sender.view!.center = CGPoint(x: sender.view!.center.x + translation.x, y: sender.view!.center.y + translation.y)
-        
-        sender.setTranslation(.zero, in: self.view)
+        if self.isZooming && sender.state == .began {
+            self.originalImageCenter = sender.view?.center
+        } else if self.isZooming && sender.state == .changed {
+            let translation = sender.translation(in: chartImageView)
+            if let view = sender.view {
+                view.center = CGPoint(x:view.center.x + translation.x,
+                                      y:view.center.y + translation.y)
+            }
+            sender.setTranslation(CGPoint.zero, in: self.chartImageView.superview)
+        }
         
     }
     override func didReceiveMemoryWarning() {
@@ -52,5 +110,6 @@ class ChartImageController: UIViewController {
     }
     
 }
+
 
 
